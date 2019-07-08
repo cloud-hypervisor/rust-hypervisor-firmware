8d44784ee72c34230f24426477f6ef8c80e6f2f7
Rob Bradford <robert.bradford@intel.com>
2019-07-08 14:16:25 +0100

    part: Expose partitition table
    This is needed for the EFI code to be able to report all the partition
    entries.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- src/part.rs
+++ src/part.rs
@@ -34,16 +34,17 @@ struct Header {
 }
 
 #[repr(packed)]
-struct Partition {
-    type_guid: [u8; 16],
-    _guid: [u8; 16],
-    first_lba: u64,
-    last_lba: u64,
+#[derive(Clone, Copy)]
+pub struct PartitionEntry {
+    pub type_guid: [u8; 16],
+    pub guid: [u8; 16],
+    pub first_lba: u64,
+    pub last_lba: u64,
     _flags: u64,
     _partition_name: [u32; 18],
 }
 
-impl Partition {
+impl PartitionEntry {
     fn is_efi_partition(&self) -> bool {
         // GUID is C12A7328-F81F-11D2-BA4B-00A0C93EC93B in mixed-endian
         // 0-3, 4-5, 6-7 are LE, 8-19, and 10-15 are BE
@@ -67,8 +68,7 @@ pub enum Error {
     NoEFIPartition,
 }
 
-/// Find EFI partition
-pub fn find_efi_partition(r: &SectorRead) -> Result<(u64, u64), Error> {
+pub fn get_partitions(r: &SectorRead, parts_out: &mut [PartitionEntry]) -> Result<u32, Error> {
     let mut data: [u8; 512] = [0; 512];
     match r.read(1, &mut data) {
         Ok(_) => {}
@@ -87,10 +87,13 @@ pub fn find_efi_partition(r: &SectorRead) -> Result<(u64, u64), Error> {
         return Err(Error::ViolatesSpecification);
     }
 
-    let mut checked_part_count = 0u32;
     let part_count = h.part_count;
+    let mut checked_part_count = 0;
+
     let first_usable_lba = h.first_usable_lba;
     let first_part_lba = h.first_part_lba;
+
+    let mut current_part = 0u32;
 
     for lba in first_part_lba..first_usable_lba {
         match r.read(lba, &mut data) {
@@ -99,16 +102,39 @@ pub fn find_efi_partition(r: &SectorRead) -> Result<(u64, u64), Error> {
         }
 
         // Safe as size of partition struct * 4 is 512 bytes (size of data)
-        let parts = unsafe { core::slice::from_raw_parts(data.as_ptr() as *const Partition, 4) };
+        let parts =
+            unsafe { core::slice::from_raw_parts(data.as_ptr() as *const PartitionEntry, 4) };
 
         for p in parts {
-            if p.is_efi_partition() {
-                return Ok((p.first_lba, p.last_lba));
+            if p.guid == [0; 16] {
+                continue;
             }
-            checked_part_count += 1;
-            if checked_part_count == part_count {
-                return Err(Error::ExceededPartitionCount);
-            }
+            parts_out[current_part as usize] = *p;
+            current_part += 1;
+        }
+
+        checked_part_count += 4;
+        if checked_part_count >= part_count {
+            break;
+        }
+    }
+
+    Ok(current_part)
+}
+
+/// Find EFI partition
+pub fn find_efi_partition(r: &SectorRead) -> Result<(u64, u64), Error> {
+    // Assume no more than 16 partitions on the disk
+    let mut parts: [PartitionEntry; 16] = unsafe { core::mem::zeroed() };
+
+    let part_count = get_partitions(r, &mut parts)? as usize;
+
+    for (checked_part_count, p) in (parts[0..part_count]).iter().enumerate() {
+        if p.is_efi_partition() {
+            return Ok((p.first_lba, p.last_lba));
+        }
+        if checked_part_count == part_count {
+            return Err(Error::ExceededPartitionCount);
         }
     }
 
