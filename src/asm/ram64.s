fb514efca3644f65b522f4be62c75db05a345f7c
Joe Richey <joerichey@google.com>
2020-02-28 15:02:46 -0800

    x86_64: Use Rust to setup SSE2
    Now that we are using the x86_64 crate, we don't need ASM to set the
    flags on the CR0 and CR4 registers. This also makes the code much more
    readable.
    
    Signed-off-by: Joe Richey <joerichey@google.com>

--- src/asm/ram64.s
+++ src/asm/ram64.s
@@ -8,17 +8,6 @@ ram64_start:
     movb $'L', %al
     outb %al, %dx
 
-    # Enable SSE2 for XMM registers (needed for EFI calling)
-    # Clear CR0.EM and Set CR0.MP
-    movq %cr0, %rax
-    andb $0b11111011, %al # Clear bit 2
-    orb  $0b00000010, %al # Set bit 1
-    movq %rax, %cr0
-    # Set CR4.OSFXSR and CR4.OSXMMEXCPT
-    movq %cr4, %rax
-    orb  $0b00000110, %ah # Set bits 9 and 10
-    movq %rax, %cr4
-
     # Setup the stack (at the end of our RAM region)
     movq $ram_max, %rsp
 
