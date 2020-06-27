32c82b8d84d1d34f3ded45e5d92ab61e085c6e2a
Joe Richey <joerichey@google.com>
2020-06-27 01:30:53 -0700

    integration: Cleanup scripts and Rust binary
    We remove unnecssary dependancies, simplifying our Cargo.lock file.
    
    We also cleanup the integration test script, to avoid writing to the
    user's home directory. This allows normal users to actually run
    ./run_integration_tests.sh
    
    We also add checks to avoid doing unnecessary work.
    
    Signed-off-by: Joe Richey <joerichey@google.com>

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,66 +1,72 @@
 #!/bin/bash
-set -x
+set -xeuf
 
-source $HOME/.cargo/env
+source "${CARGO_HOME:-$HOME/.cargo}/env"
+
+XBUILD_VERSION="0.5.34"
+cargo install cargo-xbuild --version $XBUILD_VERSION
+
+rustup component add rust-src
+cargo xbuild --release --target target.json
 
 CH_VERSION="v0.8.0"
-rm cloud-hypervisor
-wget --quiet "https://github.com/cloud-hypervisor/cloud-hypervisor/releases/download/$CH_VERSION/cloud-hypervisor" || exit 1
-chmod +x cloud-hypervisor
+CH_URL="https://github.com/cloud-hypervisor/cloud-hypervisor/releases/download/$CH_VERSION/cloud-hypervisor"
+CH_PATH="./resources/cloud-hypervisor"
+if [ ! -f "$CH_PATH" ]; then
+    wget --quiet $CH_URL -O $CH_PATH
+    chmod +x $CH_PATH
+fi
 
-WORKLOADS_DIR="$HOME/workloads"
-mkdir -p "$WORKLOADS_DIR"
-
+IMAGES_DIR="./resources/images"
+mkdir -p "$IMAGES_DIR"
 
 CLEAR_OS_IMAGE_NAME="clear-31311-cloudguest.img"
 CLEAR_OS_IMAGE_URL="https://cloudhypervisorstorage.blob.core.windows.net/images/$CLEAR_OS_IMAGE_NAME"
-CLEAR_OS_IMAGE="$WORKLOADS_DIR/$CLEAR_OS_IMAGE_NAME"
+CLEAR_OS_IMAGE="$IMAGES_DIR/$CLEAR_OS_IMAGE_NAME"
 if [ ! -f "$CLEAR_OS_IMAGE" ]; then
-    pushd $WORKLOADS_DIR
-    wget --quiet $CLEAR_OS_IMAGE_URL || exit 1
+    pushd $IMAGES_DIR
+    wget --quiet $CLEAR_OS_IMAGE_URL
     popd
 fi
 
 BIONIC_OS_IMAGE_NAME="bionic-server-cloudimg-amd64.img"
 BIONIC_OS_IMAGE_URL="https://cloudhypervisorstorage.blob.core.windows.net/images/$BIONIC_OS_IMAGE_NAME"
-BIONIC_OS_IMAGE="$WORKLOADS_DIR/$BIONIC_OS_IMAGE_NAME"
+BIONIC_OS_IMAGE="$IMAGES_DIR/$BIONIC_OS_IMAGE_NAME"
 if [ ! -f "$BIONIC_OS_IMAGE" ]; then
-    pushd $WORKLOADS_DIR
-    wget --quiet $BIONIC_OS_IMAGE_URL || exit 1
+    pushd $IMAGES_DIR
+    wget --quiet $BIONIC_OS_IMAGE_URL
     popd
 fi
 
 BIONIC_OS_RAW_IMAGE_NAME="bionic-server-cloudimg-amd64-raw.img"
-BIONIC_OS_RAW_IMAGE="$WORKLOADS_DIR/$BIONIC_OS_RAW_IMAGE_NAME"
+BIONIC_OS_RAW_IMAGE="$IMAGES_DIR/$BIONIC_OS_RAW_IMAGE_NAME"
 if [ ! -f "$BIONIC_OS_RAW_IMAGE" ]; then
-    pushd $WORKLOADS_DIR
-    qemu-img convert -p -f qcow2 -O raw $BIONIC_OS_IMAGE_NAME $BIONIC_OS_RAW_IMAGE_NAME || exit 1
+    pushd $IMAGES_DIR
+    qemu-img convert -p -f qcow2 -O raw $BIONIC_OS_IMAGE_NAME $BIONIC_OS_RAW_IMAGE_NAME
     popd
 fi
 
 
 FOCAL_OS_IMAGE_NAME="focal-server-cloudimg-amd64.img"
 FOCAL_OS_IMAGE_URL="https://cloudhypervisorstorage.blob.core.windows.net/images/$FOCAL_OS_IMAGE_NAME"
-FOCAL_OS_IMAGE="$WORKLOADS_DIR/$FOCAL_OS_IMAGE_NAME"
+FOCAL_OS_IMAGE="$IMAGES_DIR/$FOCAL_OS_IMAGE_NAME"
 if [ ! -f "$FOCAL_OS_IMAGE" ]; then
-    pushd $WORKLOADS_DIR
-    wget --quiet $FOCAL_OS_IMAGE_URL || exit 1
+    pushd $IMAGES_DIR
+    wget --quiet $FOCAL_OS_IMAGE_URL
     popd
 fi
 
 FOCAL_OS_RAW_IMAGE_NAME="focal-server-cloudimg-amd64-raw.img"
-FOCAL_OS_RAW_IMAGE="$WORKLOADS_DIR/$FOCAL_OS_RAW_IMAGE_NAME"
+FOCAL_OS_RAW_IMAGE="$IMAGES_DIR/$FOCAL_OS_RAW_IMAGE_NAME"
 if [ ! -f "$FOCAL_OS_RAW_IMAGE" ]; then
-    pushd $WORKLOADS_DIR
-    qemu-img convert -p -f qcow2 -O raw $FOCAL_OS_IMAGE_NAME $FOCAL_OS_RAW_IMAGE_NAME || exit 1
+    pushd $IMAGES_DIR
+    qemu-img convert -p -f qcow2 -O raw $FOCAL_OS_IMAGE_NAME $FOCAL_OS_RAW_IMAGE_NAME
     popd
 fi
 
-cargo install cargo-xbuild
-rustup component add rust-src
-cargo xbuild --release --target target.json
+# Add the user to the kvm group (if not already in it), so they can run VMs
+id -nGz "$USER" | grep -qzxF kvm || sudo adduser "$USER" kvm
 
-sudo adduser $USER kvm
 newgrp kvm << EOF
 export RUST_BACKTRACE=1
 cargo test --features "integration_tests"  -- --test-threads=1 test_boot
