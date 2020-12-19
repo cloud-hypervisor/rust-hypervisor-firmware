35a6182c858681af3b33ae3aaeb271d44dfe4ebc
Akira Moroo <retrage01@gmail.com>
2020-12-19 20:22:44 +0900

    CI: Add fetch_disk)images.sh
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -14,52 +14,7 @@ if [ ! -f "$CH_PATH" ]; then
     chmod +x $CH_PATH
 fi
 
-IMAGES_DIR="./resources/images"
-mkdir -p "$IMAGES_DIR"
-
-CLEAR_OS_IMAGE_NAME="clear-31311-cloudguest.img"
-CLEAR_OS_IMAGE_URL="https://cloudhypervisorstorage.blob.core.windows.net/images/$CLEAR_OS_IMAGE_NAME"
-CLEAR_OS_IMAGE="$IMAGES_DIR/$CLEAR_OS_IMAGE_NAME"
-if [ ! -f "$CLEAR_OS_IMAGE" ]; then
-    pushd $IMAGES_DIR
-    wget --quiet $CLEAR_OS_IMAGE_URL
-    popd
-fi
-
-BIONIC_OS_IMAGE_NAME="bionic-server-cloudimg-amd64.img"
-BIONIC_OS_IMAGE_URL="https://cloudhypervisorstorage.blob.core.windows.net/images/$BIONIC_OS_IMAGE_NAME"
-BIONIC_OS_IMAGE="$IMAGES_DIR/$BIONIC_OS_IMAGE_NAME"
-if [ ! -f "$BIONIC_OS_IMAGE" ]; then
-    pushd $IMAGES_DIR
-    wget --quiet $BIONIC_OS_IMAGE_URL
-    popd
-fi
-
-BIONIC_OS_RAW_IMAGE_NAME="bionic-server-cloudimg-amd64-raw.img"
-BIONIC_OS_RAW_IMAGE="$IMAGES_DIR/$BIONIC_OS_RAW_IMAGE_NAME"
-if [ ! -f "$BIONIC_OS_RAW_IMAGE" ]; then
-    pushd $IMAGES_DIR
-    qemu-img convert -p -f qcow2 -O raw $BIONIC_OS_IMAGE_NAME $BIONIC_OS_RAW_IMAGE_NAME
-    popd
-fi
-
-
-FOCAL_OS_IMAGE_NAME="focal-server-cloudimg-amd64.img"
-FOCAL_OS_IMAGE_URL="https://cloudhypervisorstorage.blob.core.windows.net/images/$FOCAL_OS_IMAGE_NAME"
-FOCAL_OS_IMAGE="$IMAGES_DIR/$FOCAL_OS_IMAGE_NAME"
-if [ ! -f "$FOCAL_OS_IMAGE" ]; then
-    pushd $IMAGES_DIR
-    wget --quiet $FOCAL_OS_IMAGE_URL
-    popd
-fi
-
-FOCAL_OS_RAW_IMAGE_NAME="focal-server-cloudimg-amd64-raw.img"
-FOCAL_OS_RAW_IMAGE="$IMAGES_DIR/$FOCAL_OS_RAW_IMAGE_NAME"
-if [ ! -f "$FOCAL_OS_RAW_IMAGE" ]; then
-    pushd $IMAGES_DIR
-    qemu-img convert -p -f qcow2 -O raw $FOCAL_OS_IMAGE_NAME $FOCAL_OS_RAW_IMAGE_NAME
-    popd
-fi
+bash ./fetch_disk_images.sh
 
 # Add the user to the kvm group (if not already in it), so they can run VMs
 id -nGz "$USER" | grep -qzxF kvm || sudo adduser "$USER" kvm
