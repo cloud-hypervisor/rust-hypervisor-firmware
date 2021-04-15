2cbc59beca14550293ed3b9d448110c66781ae34
Akira Moroo <retrage01@gmail.com>
2021-04-15 18:07:03 +0900

    tests: Add QEMU coreboot Windows tests
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- src/integration.rs
+++ src/integration.rs
@@ -227,6 +227,12 @@ mod tests {
     }
 
     #[derive(Debug)]
+    struct PasswordAuth {
+        username: String,
+        password: String,
+    }
+
+    #[derive(Debug)]
     enum SSHCommandError {
         Connection(std::io::Error),
         Handshake(ssh2::Error),
@@ -235,7 +241,11 @@ mod tests {
         Command(ssh2::Error),
     }
 
-    fn ssh_command(ip: &str, command: &str) -> Result<String, SSHCommandError> {
+    fn ssh_command_with_auth(
+        ip: &str,
+        command: &str,
+        auth: &PasswordAuth,
+    ) -> Result<String, SSHCommandError> {
         const DEFAULT_SSH_RETRIES: u8 = 6;
         const DEFAULT_SSH_TIMEOUT: u8 = 10;
 
@@ -252,7 +262,7 @@ mod tests {
                 sess.set_tcp_stream(tcp);
                 sess.handshake().map_err(SSHCommandError::Handshake)?;
 
-                sess.userauth_password("cloud", "cloud123")
+                sess.userauth_password(&auth.username, &auth.password)
                     .map_err(SSHCommandError::Authentication)?;
                 assert!(sess.authenticated());
 
@@ -279,6 +289,17 @@ mod tests {
             thread::sleep(std::time::Duration::new((timeout * counter).into(), 0));
         }
         Ok(s)
+    }
+
+    fn ssh_command(ip: &str, command: &str) -> Result<String, SSHCommandError> {
+        ssh_command_with_auth(
+            ip,
+            command,
+            &PasswordAuth {
+                username: String::from("cloud"),
+                password: String::from("cloud123"),
+            },
+        )
     }
 
     fn spawn_ch(os: &str, ci: &str, net: &GuestNetworkConfig) -> Child {
@@ -435,5 +456,70 @@ mod tests {
     #[cfg(not(feature = "coreboot"))]
     fn test_boot_ch_clear() {
         test_boot(CLEAR_IMAGE_NAME, &ClearCloudInit {}, spawn_ch)
+    }
+
+    const WINDOWS_IMAGE_NAME: &str = "windows-server-2019.raw";
+
+    fn windows_auth() -> PasswordAuth {
+        PasswordAuth {
+            username: String::from("administrator"),
+            password: String::from("Admin123"),
+        }
+    }
+
+    #[test]
+    #[cfg(feature = "coreboot")]
+    fn test_boot_qemu_windows() {
+        let tmp_dir = TempDir::new().expect("Expect creating temporary directory to succeed");
+        let net = GuestNetworkConfig::new(COUNTER.fetch_add(1, Ordering::SeqCst) as u8);
+        let os = prepare_os_disk(&tmp_dir, WINDOWS_IMAGE_NAME);
+
+        prepare_tap(&net);
+
+        let fw = Firmware {
+            fw_type: "-bios",
+            path: "resources/coreboot/coreboot/build/coreboot.rom",
+        };
+
+        let mut c = Command::new("qemu-system-x86_64");
+        c.args(&[
+            "-machine",
+            "q35,accel=kvm",
+            "-cpu",
+            "host,-vmx",
+            fw.fw_type,
+            fw.path,
+            "-display",
+            "none",
+            "-nodefaults",
+            "-serial",
+            "stdio",
+            "-drive",
+            &format!("id=os,file={},if=none", os),
+            "-device",
+            "virtio-blk-pci,drive=os,disable-legacy=on",
+            "-m",
+            "4G",
+            "-netdev",
+            &format!(
+                "tap,id=net0,ifname={},script=no,downscript=no",
+                net.tap_name
+            ),
+            "-device",
+            &format!("virtio-net-pci,netdev=net0,mac={}", net.guest_mac),
+        ]);
+
+        eprintln!("Spawning: {:?}", c);
+        let mut child = c.spawn().expect("Expect launching QEMU to succeed");
+
+        thread::sleep(std::time::Duration::from_secs(60));
+        let auth = windows_auth();
+        ssh_command_with_auth(&net.guest_ip, "shutdown /s", &auth)
+            .expect("Expect SSH command to work");
+
+        child.kill().unwrap();
+        child.wait().unwrap();
+
+        cleanup_tap(&net);
     }
 }
