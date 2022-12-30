9d25f479ddfbf2236966595864802eb617675c11
Akira Moroo <retrage01@gmail.com>
2022-12-30 18:47:58 +0900

    build: Change to use aarch64-cpu crate
    "cortex-a" crate has been renamed to "aarch64-cpu", and is deprecated.
    This commit replace this crate with "aarch64-cpu" crate.
    
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- src/arch/aarch64/paging.rs
+++ src/arch/aarch64/paging.rs
@@ -4,7 +4,7 @@
 
 use core::ops::RangeInclusive;
 
-use cortex_a::{asm::barrier, registers::*};
+use aarch64_cpu::{asm::barrier, registers::*};
 use tock_registers::interfaces::{ReadWriteable, Readable, Writeable};
 
 use self::interface::Mmu;
