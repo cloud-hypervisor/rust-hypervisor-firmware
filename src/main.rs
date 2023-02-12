76730195dbf2fb28df60f67323818d2353cc48b9
Akira Moroo <retrage01@gmail.com>
2023-02-12 14:01:05 +0900

    build: Remove `abi_efiapi` feature attribute
    This feature has been stabilized from 1.68.0.
    
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(abi_efiapi)]
 #![feature(asm_const)]
 #![feature(alloc_error_handler)]
 #![feature(stmt_expr_attributes)]
