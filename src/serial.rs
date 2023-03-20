0832a48b0ec746eeab8e698ce261313b59728bad
Rob Bradford <rbradford@rivosinc.com>
2023-03-20 14:28:06 +0000

    serial: riscv: Add basic MMIO UART device
    This device doe not fully implement a UART driver but provides
    sufficient support to enable the log!() macro and EFI stdout support
    when running on the the QEMU virt platform.
    
    Signed-off-by: Rob Bradford <rbradford@rivosinc.com>

--- src/serial.rs
+++ src/serial.rs
@@ -25,6 +25,9 @@ use crate::{arch::aarch64::layout::map, uart_pl011::Pl011 as UartPl011};
 #[cfg(target_arch = "x86_64")]
 use uart_16550::SerialPort as Uart16550;
 
+#[cfg(target_arch = "riscv64")]
+use crate::uart_mmio::UartMmio;
+
 // We use COM1 as it is the standard first serial port.
 #[cfg(target_arch = "x86_64")]
 pub static PORT: AtomicRefCell<Uart16550> = AtomicRefCell::new(unsafe { Uart16550::new(0x3f8) });
@@ -32,6 +35,12 @@ pub static PORT: AtomicRefCell<Uart16550> = AtomicRefCell::new(unsafe { Uart1655
 #[cfg(target_arch = "aarch64")]
 pub static PORT: AtomicRefCell<UartPl011> =
     AtomicRefCell::new(UartPl011::new(map::mmio::PL011_START));
+
+// TODO: Fill from FDT?
+#[cfg(target_arch = "riscv64")]
+const SERIAL_PORT_ADDRESS: u64 = 0x1000_0000;
+#[cfg(target_arch = "riscv64")]
+pub static PORT: AtomicRefCell<UartMmio> = AtomicRefCell::new(UartMmio::new(SERIAL_PORT_ADDRESS));
 
 pub struct Serial;
 impl fmt::Write for Serial {
