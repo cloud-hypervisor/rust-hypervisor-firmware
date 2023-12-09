bd604a9148ea7abd06a81fb5232bfc99d2acda5a
Akira Moroo <retrage01@gmail.com>
2023-12-09 08:50:53 +0000

    build: Watch riscv64 target json and linker script
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 fn main() {
     println!("cargo:rerun-if-changed=aarch64-unknown-none.json");
     println!("cargo:rerun-if-changed=aarch64-unknown-none.ld");
+    println!("cargo:rerun-if-changed=riscv64gcv-unknown-none-elf.json");
+    println!("cargo:rerun-if-changed=riscv64gcv-unknown-none-elf.ld");
     println!("cargo:rerun-if-changed=x86_64-unknown-none.json");
     println!("cargo:rerun-if-changed=x86_64-unknown-none.ld");
 }
