7e6857c9dc426e50cf665d68f715f9b6790f7bdb
Rob Bradford <rbradford@rivosinc.com>
2023-12-19 15:08:48 +0000

    efi: Add support for memory mapped device path
    Signed-off-by: Rob Bradford <rbradford@rivosinc.com>

--- src/efi/mod.rs
+++ src/efi/mod.rs
@@ -940,6 +940,7 @@ extern "efiapi" fn image_unload(_: Handle) -> Status {
 #[allow(clippy::large_enum_variant)]
 enum DevicePath {
     File([u8; 256]),
+    Memory(MemoryType, u64, u64),
     Unsupported,
 }
 
@@ -954,6 +955,24 @@ impl DevicePath {
                 crate::common::ucs2_to_ascii(ptr, &mut path);
                 return DevicePath::File(path);
             }
+            if dpp.r#type == r_efi::protocols::device_path::TYPE_HARDWARE
+                && dpp.sub_type == r_efi::protocols::device_path::Hardware::SUBTYPE_MMAP
+            {
+                let memory_type_ptr = (dpp as *const _ as usize
+                    + offset_of!(MemoryDevicePathProtocol, memory_type))
+                    as *const MemoryType;
+                let start_ptr = (dpp as *const _ as usize
+                    + offset_of!(MemoryDevicePathProtocol, start))
+                    as *const u64;
+                let end_ptr = (dpp as *const _ as usize + offset_of!(MemoryDevicePathProtocol, end))
+                    as *const u64;
+                return DevicePath::Memory(
+                    unsafe { *memory_type_ptr },
+                    unsafe { *start_ptr },
+                    unsafe { *end_ptr },
+                );
+            }
+
             if dpp.r#type == r_efi::protocols::device_path::TYPE_END && dpp.sub_type == 0xff {
                 log!("Unexpected end of device path");
                 return DevicePath::Unsupported;
@@ -966,6 +985,7 @@ impl DevicePath {
     fn generate(&self) -> *mut r_efi::protocols::device_path::Protocol {
         match self {
             Self::File(path) => file_device_path(crate::common::ascii_strip(path)),
+            Self::Memory(memory_type, start, end) => memory_device_path(*memory_type, *start, *end),
             Self::Unsupported => panic!("Cannot generate from unsupported Device Path type"),
         }
     }
@@ -1090,6 +1110,55 @@ fn file_device_path(path: &str) -> *mut r_efi::protocols::device_path::Protocol 
     crate::common::ascii_to_ucs2(path, &mut file_paths[0].filename);
 
     &mut file_paths[0].device_path // Pointer to first path entry
+}
+
+#[repr(C)]
+struct MemoryDevicePathProtocol {
+    pub device_path: DevicePathProtocol,
+    pub memory_type: u32,
+    pub start: u64,
+    pub end: u64,
+}
+
+type MemoryDevicePaths = [MemoryDevicePathProtocol; 2];
+
+fn memory_device_path(
+    memory_type: MemoryType,
+    start: u64,
+    end: u64,
+) -> *mut r_efi::protocols::device_path::Protocol {
+    let mut memory_paths = null_mut();
+    let status = allocate_pool(
+        efi::LOADER_DATA,
+        size_of::<MemoryDevicePaths>(),
+        &mut memory_paths as *mut *mut c_void,
+    );
+    assert!(status == Status::SUCCESS);
+    let memory_paths = unsafe { &mut *(memory_paths as *mut MemoryDevicePaths) };
+    *memory_paths = [
+        MemoryDevicePathProtocol {
+            device_path: DevicePathProtocol {
+                r#type: r_efi::protocols::device_path::TYPE_HARDWARE,
+                sub_type: r_efi::protocols::device_path::Hardware::SUBTYPE_MMAP,
+                length: (size_of::<MemoryDevicePathProtocol>() as u16).to_le_bytes(),
+            },
+            memory_type,
+            start,
+            end,
+        },
+        MemoryDevicePathProtocol {
+            device_path: DevicePathProtocol {
+                r#type: r_efi::protocols::device_path::TYPE_END,
+                sub_type: r_efi::protocols::device_path::End::SUBTYPE_ENTIRE,
+                length: (size_of::<DevicePathProtocol>() as u16).to_le_bytes(),
+            },
+            memory_type: 0,
+            start: 0,
+            end: 0,
+        },
+    ];
+
+    &mut memory_paths[0].device_path // Pointer to first path entry
 }
 
 fn new_image_handle(
